9f9702ec03c4845cef3bd0b5609eaec6a9abd6ce
Eragonaz <muhammadfikri6844@gmail.com>
2021-11-04 10:35:43 +0800

    Added assertion connected checks
    Now it asserts when user that aren't connected to the same voice channel as Boomby tries to use the commands

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+from random import random
 from threading import local
 import discord
 import time
@@ -65,6 +66,31 @@ async def help(ctx):
     embed.add_field(name='clear', value='Clears message(s)', inline=False)
 
     await ctx.send(embed=embed)
+# ------------------------------------- - ------------------------------------ #
+
+# -------------------------------- isConnected ------------------------------- #
+async def is_connected(ctx, user_connected = True, send_message = False):
+    if user_connected and not ctx.author.voice:
+        if send_message:
+            await ctx.send(':angry: You are not in a voice channel!')
+        return False
+
+    channel = ctx.message.author.voice.channel
+    voice = get(client.voice_clients, guild=ctx.guild)
+    guild_id = ctx.message.guild.id
+    
+    if voice and voice.is_connected():
+        
+        if user_connected and voice.channel != ctx.message.author.voice.channel:
+            if send_message:
+                await ctx.send(':angry: You are not in the same voice channel as Boomby!')
+            return False
+        
+        return True
+
+    if send_message:
+        await ctx.send(':confused: Boomby is not in a voice channel.. Use !join first.')
+    return False
 # ------------------------------------- - ------------------------------------ #
 
 # ---------------------------------- play, p --------------------------------- #
@@ -135,19 +161,20 @@ async def p(ctx, *, url):
 
 # -------------------------------- remove, rm -------------------------------- #
 async def fremove(ctx, index=0):
-    if ctx.message.guild.id in music_queues:
-        local_queue = music_queues[ctx.message.guild.id]
-        index = int(index)
-        
-        if index == 0:
-            index = len(local_queue)
-        else:
-            index -= 1
-        
-        music_data = local_queue[index]
-        if music_data:
-            local_queue.pop(index)
-            await ctx.send("Removed: " + music_data['title'] + " from queue!")    
+    if await is_connected(ctx, user_connected=True, send_message=True):
+        if ctx.message.guild.id in music_queues:
+            local_queue = music_queues[ctx.message.guild.id]
+            index = int(index)
+            
+            if index == 0:
+                index = len(local_queue)
+            else:
+                index -= 1
+            
+            music_data = local_queue[index]
+            if music_data:
+                local_queue.pop(index)
+                await ctx.send("Removed: " + music_data['title'] + " from queue!")    
 
 @client.command(pass_context = True)
 async def remove(ctx, index):
@@ -159,10 +186,8 @@ async def rm(ctx, index):
 
 # --------------------------------- pause, ps -------------------------------- #
 async def fpause(ctx):
-    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-    if not (ctx.author.voice):
-        await ctx.send(':angry; You are not in voice channel!')
-    else:  
+    if await is_connected(ctx, user_connected=True, send_message=True):
+        voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
         if voice.is_playing():
             voice.pause()
             await ctx.send(':pause_button: Paused: ' + currently_playing[ctx.message.guild.id]['title'])
@@ -179,10 +204,8 @@ async def ps(ctx):
 
 # --------------------------------- resume, r -------------------------------- #
 async def fresume(ctx):
-    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-    if not (ctx.author.voice):
-        await ctx.send(':angry: You are not in voice channel!')
-    else:  
+    if await is_connected(ctx, user_connected=True, send_message=True):
+        voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
         if voice.is_paused():
             voice.resume()
             await ctx.send(':play_pause: Resumed: ' + currently_playing[ctx.message.guild.id]['title'])
@@ -199,12 +222,9 @@ async def r(ctx):
 
 # ---------------------------------- skip, s --------------------------------- #
 async def fskip(ctx):
-    global currently_playing
-    
-    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-    if not (ctx.author.voice):
-        await ctx.send(':angry: You are not in a voice-channel!')
-    else:
+    if await is_connected(ctx, user_connected=True, send_message=True):
+        global currently_playing    
+        voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
         if voice.is_playing():
             voice.stop()
             await ctx.send(':track_next: Skipped: ' + currently_playing[ctx.message.guild.id]['title'])
@@ -222,20 +242,21 @@ async def s(ctx):
 
 # --------------------------------- queue, q --------------------------------- #
 async def fqueue(ctx):
-    guild_id = ctx.message.guild.id
-    if not currently_playing[guild_id] or currently_playing[guild_id] == null_music_data:
-        await ctx.send(':cricket: Nothing playing or in queue')
-    else:
-        embed = discord.Embed(colour = discord.Colour.magenta())
-        embed.add_field(name=':microphone: Now playing: ' + currently_playing[guild_id]['title'], value='Requested by :' + currently_playing[guild_id]['requestor'].mention, inline=False)
-        embed.add_field(name='Up next:', value=':parrot:', inline = False)
-        if ctx.message.guild.id in music_queues:
-            local_queue = music_queues[ctx.message.guild.id]
-            for i in range(len(local_queue)):
-                music_data = local_queue[i]
-                embed.add_field(name=str((i+1))+') ' + music_data['title'], value='Requested by: ' + music_data['requestor'].mention, inline=False)
+    if await is_connected(ctx, user_connected=True, send_message=True):
+        guild_id = ctx.message.guild.id
+        if not currently_playing[guild_id] or currently_playing[guild_id] == null_music_data:
+            await ctx.send(':cricket: Nothing playing or in queue')
+        else:
+            embed = discord.Embed(colour = discord.Colour.magenta())
+            embed.add_field(name=':microphone: Now playing: ' + currently_playing[guild_id]['title'], value='Requested by :' + currently_playing[guild_id]['requestor'].mention, inline=False)
+            embed.add_field(name='Up next:', value=':parrot:', inline = False)
+            if ctx.message.guild.id in music_queues:
+                local_queue = music_queues[ctx.message.guild.id]
+                for i in range(len(local_queue)):
+                    music_data = local_queue[i]
+                    embed.add_field(name=str((i+1))+') ' + music_data['title'], value='Requested by: ' + music_data['requestor'].mention, inline=False)
 
-        await ctx.send(embed = embed)
+            await ctx.send(embed = embed)
 
 @client.command(pass_context = True)
 async def queue(ctx):
@@ -258,10 +279,10 @@ async def clear_queue(ctx, clear_active = False):
     await ctx.send('Queue has been cleared!')
 
 async def fstop(ctx):
-    await clear_queue(ctx, True)
-    await ctx.guild.voice_client.disconnect()
+    if await is_connected(ctx, user_connected=True, send_message=True):
+        await clear_queue(ctx, True)
+        await ctx.guild.voice_client.disconnect()
 
-    if (ctx.author.voice):
         channel = ctx.message.author.voice.channel
         voice = get(client.voice_clients, guild=ctx.guild)
         if voice and voice.is_connected():
@@ -279,13 +300,13 @@ async def st(ctx):
 # ------------------------------------- - ------------------------------------ #
 
 # --------------------------------- join, disconnect --------------------------------- #
-        
 @client.command(pass_context = True)
 async def join(ctx):
     if (ctx.author.voice):
         channel = ctx.message.author.voice.channel
         voice = get(client.voice_clients, guild=ctx.guild)
-
+        guild_id = ctx.message.guild.id
+        
         #await clear_queue(ctx)
         if voice and voice.is_connected():
             if voice.channel != channel:
@@ -295,7 +316,9 @@ async def join(ctx):
                 return
         else:
             voice = await channel.connect()      
-    
+
+        currently_playing[guild_id] = null_music_data
+        music_queues[guild_id] = []
         await ctx.send(':dancer: Joined!')
     else:
         await ctx.send(':angry: You are not in a voice channel')
@@ -303,12 +326,10 @@ async def join(ctx):
 
 @client.command(pass_context = True)
 async def disconnect(ctx):
-    if (ctx.voice_client):
+    if await is_connected(ctx, user_connected=True, send_message=True):
         await clear_queue(ctx, True)
         await ctx.guild.voice_client.disconnect()
         await ctx.send(':smiling_face_with_tear: Leaving')
-    else:
-        await ctx.send(':confused: Boomby is not in a voice channel')
 # ------------------------------------- - ------------------------------------ #
 
 # ----------------------------------- clear ---------------------------------- #
@@ -316,9 +337,17 @@ async def disconnect(ctx):
 async def clear(ctx, count=5):
     await ctx.channel.purge(limit=count+1)
     await ctx.send(':x: Messages deleted!')
-    time.sleep(5)
+    time.sleep(1)
     await ctx.channel.purge(limit=1)    
 # ------------------------------------- - ------------------------------------ #
+
+# ------------------------------------ zq ------------------------------------ #
+st_r = "ziqian likes constance"
+@client.command()
+async def zqlikes(ctx):
+    for i in range(len(st_r)):
+        await ctx.send(st_r[0:i+1])
+        time.sleep(0.05)
 
 
 keep_alive()
